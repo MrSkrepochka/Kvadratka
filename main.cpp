ff205ade628e1732444dbcbf4be4cca691eed845
MrSkrepochka <evgeniy1219@mail.ru>
2025-08-25 18:37:25 +0300

    test functions added

--- main.cpp
+++ main.cpp
@@ -1,8 +1,9 @@
 #include <stdio.h>
 #include <math.h>
 #include <ctype.h>
+#include <assert.h>
 
-struct сoefficients {
+struct Coefficients {
     double a;
     double b;
     double c;
@@ -15,122 +16,210 @@ enum RootsNumber  {
     INFINITE_ROOTS,
 };
 
+struct RootsData {
+    RootsNumber number;
+    double x1;
+    double x2;
+};
+
 const double EPS =1e-6;
 
-void PrintOutput (int nRoots, double x1, double x2);
-bool ReadInput ( сoefficients *kf, double a,  double b, double c);
-RootsNumber SolveEquation (сoefficients kf, double *x1, double *x2);
-RootsNumber SolveLinear ( double *x, сoefficients kf);
-RootsNumber SolveQuadratic (сoefficients kf, double *x1, double *x2);
+void AllTests ();
+
+void PrintOutput (RootsData root);
+bool ReadInput (Coefficients *coefficient);
+void RunTest (double a, double b, double c, Coefficients coefficient, RootsData root, int nRoots, double x1, double x2);
+RootsNumber SolveEquation (Coefficients coefficient,  RootsData *root);
+RootsNumber SolveLinear (RootsData *root, Coefficients coefficient);
+RootsNumber SolveQuadratic (Coefficients coefficient, RootsData *root);
 
 int main()
 {
-    double a = 0, b = 0, c = 0;
-    struct сoefficients kf; // структура для хранения значений коэффициентов a, b, c
-    double x1 = NAN, x2 = NAN; 
+    RootsData root;
+    Coefficients coefficient; 
+    AllTests ();
 
-    if (ReadInput( &kf, a, b, c) == false )
+    if (!ReadInput( &coefficient ))
     {
         printf("Неверный ввод\n");
         return 0;
     }
 
-    int nRoots = SolveEquation (kf, &x1, &x2);
-    PrintOutput(nRoots, x1, x2);
+    root.number = SolveEquation (coefficient, &root);
+    PrintOutput(root);
     
     return 0;
 }
 
-bool ReadInput (сoefficients *kf, double a, double b, double c)
+bool ReadInput (Coefficients *coefficient)
 {
+    assert( coefficient != NULL);
+
     printf("Введите значения коэффициентов уравнения\n");
     printf("вида a*x^2+b*x+c=0\n");
     printf("a, b, и с: \n");
 
-    int nInput = scanf("%lf %lf %lf", &a, &b, &c ); // как ввести через scanf сразу в kf.a, ... 
-    (*kf).a = a;                                    // и окончательно избавиться от локальных
-    (*kf).b = b;                                    // переменных a, b, c?
-    (*kf).c = c;
+    int nInput = scanf ("%lf %lf %lf", &coefficient -> a, &coefficient -> b, &coefficient -> c);
     
-    if ( isfinite ((*kf).a) == 0 || isfinite ((*kf).b) == 0 || isfinite ((*kf).c) == 0 || nInput<3 )
+    if ( isfinite ((*coefficient).a) == 0 || isfinite ((*coefficient).b) == 0 || isfinite ((*coefficient).c) == 0 || nInput<3 )
         return false;
     else
         return true;
 }
 
 
-RootsNumber SolveEquation (сoefficients kf, double *x1, double *x2) 
+RootsNumber SolveEquation (Coefficients coefficient, RootsData *r) 
 { 
-    if (fabs(kf.a) < EPS)
-        return SolveLinear (x1, kf); 
+    assert (isfinite (coefficient.a));
+    assert (isfinite (coefficient.b));
+    assert (isfinite (coefficient.c));
+
+    assert (r != NULL);
+
+    if (fabs(coefficient.a) < EPS)
+        return SolveLinear (r, coefficient); 
     else
-        return SolveQuadratic (kf, x1, x2);
+        return SolveQuadratic (coefficient, r);
 }
 
 
-RootsNumber SolveLinear (double *x1, сoefficients kf) 
+RootsNumber SolveLinear (RootsData *root, Coefficients coefficient) 
 {
-    if (fabs(kf.b)<EPS)
+    assert (isfinite (coefficient.a));
+    assert (isfinite (coefficient.b));
+    assert (isfinite (coefficient.c));
+
+    assert (root != NULL);
+
+    if (fabs (coefficient.b)<EPS)
     {
-        if ( fabs (kf.c) < EPS)
+        if (fabs (coefficient.c) < EPS)
             return INFINITE_ROOTS;
         else
+        {
+            root -> x1 = 0;
+            root -> x2 = 0;
             return ZERO_ROOTS;
+        }    
     } 
     else           
     {
-        *x1 = -kf.c/kf.b;
+        root -> x1 = -coefficient.c/coefficient.b;
+        root -> x2 = -coefficient.c/coefficient.b;
         return ONE_ROOT;
     }
 }
 
-
-RootsNumber SolveQuadratic (сoefficients kf, double *x1, double *x2 )
+RootsNumber SolveQuadratic (Coefficients coefficient, RootsData *root)
 {
-    double discriminant= kf.b * kf.b - 4* kf.a * kf.c; 
+    assert (isfinite (coefficient.a));
+    assert (isfinite (coefficient.b));
+    assert (isfinite (coefficient.c));
+
+    assert (root != NULL);
+
+    double discriminant= coefficient.b * coefficient.b - 4 * coefficient.a * coefficient.c; 
 
     if ( discriminant < -EPS)
     {
+        root -> x1 = 0;
+        root -> x2 = 0;
         return ZERO_ROOTS;
     } 
     else 
     {
         if (fabs (discriminant) < EPS)
         {
-            *x1 = -kf.b / (2 * kf.a);
+            root -> x1 = -coefficient.b / (2 * coefficient.a);
+            root -> x2 = -coefficient.b / (2 * coefficient.a);
             return ONE_ROOT;
         } 
         else
         {
-            *x1 = (-kf.b - sqrt (discriminant)) / (2 * kf.a);
-            *x2 = (-kf.b + sqrt (discriminant)) / (2 * kf.a);
+            root -> x1 = (-coefficient.b - sqrt (discriminant)) / (2 * coefficient.a);
+            root -> x2 = (-coefficient.b + sqrt (discriminant)) / (2 * coefficient.a);
             return TWO_ROOTS;
         }
     }
 }
 
+void PrintOutput (RootsData root)
+{
+    assert (isfinite (root.x1));
+    assert (isfinite (root.x2));
+    //assert (isfinite (r.number));
 
-void PrintOutput (int nRoots, double x1, double x2)
-{   
-    switch (nRoots)
+    switch (root.number)
     {
         case ZERO_ROOTS:
             printf("Уравнение не имеет решений\n");
             break;
         case TWO_ROOTS:
-            printf("x1 = %lf\n", x1);
-            printf("x2 = %lf\n", x2);
+            printf("x1 = %lf\n", root.x1);
+            printf("x2 = %lf\n", root.x2);
             break;
         case INFINITE_ROOTS:
             printf("Уравнение имеет бесконечное количество корней\n");
             break;
         case ONE_ROOT:
-            printf("Единственное решение х= %lf\n", x1);
+            printf("Единственное решение х= %lf\n", root.x1);
             break;
         default:
             printf("Неверный формат параметров вывода\n");
             break;  
-            
-        
     }
+}
+void AllTests()
+{
+    RootsData root;
+    Coefficients coefficient;
+    
+    RunTest (1, -5 , 6, coefficient, root, 2, 2, 3 );
+    RunTest (1, -5 , 6, coefficient, root, 2, 3, 2 );
+    RunTest (-1, 5, -6, coefficient, root, 2, 3, 2);
+    RunTest (1, 2, 1, coefficient, root, 1, -1, -1);
+    RunTest (1, -4, 3, coefficient, root, 2, 1, 3);
+    RunTest (1, -4, 3, coefficient, root, 2, 3, 1);
+    RunTest (1, 5 , 7, coefficient, root, 0, 0, 0);
+    RunTest (1, 0, -25, coefficient, root, 2, -5, 5);
+    RunTest (1, 0, 25, coefficient, root, 0, 0, 0);
+    RunTest (0, 5, 10, coefficient, root, 1, -2, -2);
+    RunTest (1, 5, 0, coefficient, root, 2, -5, 0);
+
+}
+
+void RunTest ( double a, double b, double c, Coefficients coefficient, RootsData root, int nRoots, double x1, double x2)
+{
+    /*assert (isfinite(a));
+    assert (isfinite(b));
+    assert (isfinite(c));
+    assert (isfinite(nRoots));
+    assert (isfinite(x1));
+    assert (isfinite(x2));*/
+    //assert (isfinite (root.number));
+
+
+
+    coefficient.a = a;
+    coefficient.b = b;
+    coefficient.c = c;
+
+    root.number = SolveEquation (coefficient, &root);
+    if (root.number == nRoots)
+    {
+        if ((fabs (root.x1 - x1) < EPS && fabs (root.x2 - x2) < EPS) || ( fabs (root.x1 - x2) < EPS && fabs (root.x2 - x1) < EPS) )
+        {
+            printf("Получен верный ответ\n");
+        }
+        else
+        {
+            printf("Получен неверный  ответ\n");
+            printf ("nRoots = %d, x1 = %lf, x2 = %lf \n", root.number, root.x1, root.x2);
+        }
+    } 
+    else
+        {
+            printf("Получен неверный  ответ\n");
+            printf ("nRoots = %d, x1 = %lf, x2 = %lf \n", root.number, root.x1, root.x2);
+        }
 }
